8c2e235bc76827358adb083a1431995c03352dc8
RuBy-67 <beyer.ruben67@gmail.com>
2023-10-31 12:59:59 +0100

    .

--- admin/admin.php
+++ admin/admin.php
@@ -22,7 +22,7 @@ ini_set('display_startup_errors', 1);
         <div class="container">
             <h2 class="text-3xl font-bold mb-4">Administration</h2>
             <div class="flex flex-wrap">
-                <div class="b-full md:w-1/2 lg:w-1/3 p-2">
+                <div class="w-full md:w-1/2 lg:w-1/3 p-2">
                     <a href="./admin/admin.php" class="bg-white shadow-md rounded-md block p-4">
                         <h3 class="text-xl font-bold mb-2">Gestion des articles</h3>
                         <p class="text-gray-600">Ajouter, modifier ou supprimer des articles.</p>
