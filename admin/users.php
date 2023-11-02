e44161750d2d162ef6d1301dc53778696919727c
RuBy-67 <beyer.ruben67@gmail.com>
2023-11-02 13:01:56 +0100

    .

--- admin/users.php
+++ admin/users.php
@@ -115,31 +115,31 @@ require '../php/functionSql.php';
     if ($result->num_rows > 0) {
       ?>
       <div>
-      <div class="ml-4">
+      <section class="container w-1/2 flex flex-col items-center my-20">
           <h4 class="text-lg font-bold mb-4">Ajouter un nouvel Utilisateur</h4>
-          <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" class="mb-4 max-w-xs">
+          <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" class="flex flex-col items-center mb-0">
             <div>
-              <input type="text" name="nom" placeholder="Nom" required class="block w-full p-2 border rounded mb-2">
-              <input type="text" name="prenom" placeholder="Prénoms" required class="block w-full p-2 border rounded mb-2">
+              <input type="text" name="nom" placeholder="Nom" required lass="mb-8 border-primary">
+              <input type="text" name="prenom" placeholder="Prénoms" required lass="mb-8 border-primary">
               <div class="relative">
-                <select name="group" class="block w-full p-2 border rounded mb-2">
+                <select name="group" lass="mb-8 border-primary">
                   <option value="0">Utilisateurs</option>
                   <option value="1">Vendeurs</option>
                   <option value="2">Admin</option>
                 </select>
               </div>
-              <input type="email" name="mail" placeholder="E-mail" required class="block w-full p-2 border rounded mb-2">
+              <input type="email" name="mail" placeholder="E-mail" required lass="mb-8 border-primary">
               <input type="number" name="numeros" placeholder="Numéro" required
-                class="block w-full p-2 border rounded mb-2">
-              <input type="text" name="rue" placeholder="Rue" required class="block w-full p-2 border rounded mb-2">
-              <input type="text" name="ville" placeholder="Ville" required class="block w-full p-2 border rounded mb-2">
-              <input type="text" name="pays" placeholder="Pays" required class="block w-full p-2 border rounded mb-2">
+                lass="mb-8 border-primary">
+              <input type="text" name="rue" placeholder="Rue" required lass="mb-8 border-primary">
+              <input type="text" name="ville" placeholder="Ville" required lass="mb-8 border-primary">
+              <input type="text" name="pays" placeholder="Pays" required lass="mb-8 border-primary">
               <input type="tel" name="telephone" placeholder="Téléphone" maxlength="10" required
-                class="block w-full p-2 border rounded mb-2">
+                lass="mb-8 border-primary">
             </div>
-            <button type="submit" name="add" class="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-700">➕</button>
+            <button type="submit" name="add" class="block bg-primary text-white px-8 py-4 flex align-center justify-center rounded">Ajouter ➕</button>
           </form>
-        </div>
+          </section>
 
 
       </div>
