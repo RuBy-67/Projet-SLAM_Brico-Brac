5b289f0db80ffd792bbb39664f693172c9004db4
RuBy-67 <beyer.ruben67@gmail.com>
2023-11-02 15:24:54 +0100

    correction margin - header

--- templates/header.php
+++ templates/header.php
@@ -29,14 +29,14 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/php/function.php');
                     echo '<a href="/pages/compte.php" class="btn mr-4 mb-4">Mon Compte</a>';
                     if ($usergroup  === 1 || $usergroup === 2) {
                         // Utilisateur connecté et a un groupe de 1 (Vendeur) ou 2 (Admin)
-                        echo '<a href="/admin/admin.php" class="btn">Gestion</a>';
+                        echo '<a href="/admin/admin.php" "btn mr-4 mb-4">Gestion</a>';
                     }
                     echo '</div>';
                 } else {
                     // Utilisateur non connecté
                     echo '<div class="flex sm:flex-row flex-col items-center">';
                     echo '<a href="/pages/sign_in.php" class="btn mr-4 mb-4">Se connecter</a>';
-                    echo '<a href="/pages/sign_up.php" class="btn">Créer un compte</a>';
+                    echo '<a href="/pages/sign_up.php" "btn mr-4 mb-4">Créer un compte</a>';
                     echo '</div>';
                 }
                 ?>
