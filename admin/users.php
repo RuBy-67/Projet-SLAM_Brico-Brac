49c99badcec2eb62ce4a5e2442b8fc6d67338792
RuBy_67 <beyer.ruben67@gmail.com>
2023-11-03 11:45:56 +0100

    fav icon admin

--- admin/users.php
+++ admin/users.php
@@ -31,7 +31,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/php/functionSql.php';
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link href="../dev/dist/output.css" rel="stylesheet">
   <link href="<?= $_SERVER['DOCUMENT_ROOT'] ?>/dev/css/splide.css" rel="stylesheet" />
-  <link rel="icon" type="image/png" href="../dev/assets/favicon.png" />
+  <link rel="icon" type="image/png" href="/dev/assets/favicon.png" />
   <link rel="stylesheet"
     href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
   <title>Brico'brac - Gestion des utilisateurs</title>
